e4786e447e8f9e7c74d88b8a8a2af5f16d2700eb
s21014 <soccer0907>
2023-01-12 10:49:17 +0900

    マップを表示する

--- app/src/main/java/jp/ac/it_college/std/s21014/Kadai/MapsActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21014/Kadai/MapsActivity.kt
@@ -29,6 +29,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             startActivity(intent)
         }
     }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -38,11 +39,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         // Add a marker in Sydney and move the camera
-        val Sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(Sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Sydney))
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
